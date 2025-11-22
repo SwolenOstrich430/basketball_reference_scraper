8fcde3e1496d6c0dd16397551a75c6ab70b65f40
SwolenOstrich430 <60279013+SwolenOstrich430@users.noreply.github.com>
2025-11-22 14:19:43 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="basketball_reference_scraper",
-    version="2.0.0",
+    version="3.0.0",
     author="Vishaal Agartha",
     author_email="vishaalagartha@gmail.com",
     license="MIT",
